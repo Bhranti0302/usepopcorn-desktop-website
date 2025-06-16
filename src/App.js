8fb06ba01bc9979fb974b9e3bd02e5d80e88353a
Bhranti0302 <bhrantitamboli40@gmail.com>
2025-06-16 20:27:04 +0530

    Fix mixed content error: Changed OMDb API calls to HTTPS

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import StarRating from "./StarRating";
 //     Title: "Inception",
 //     Year: "2010",
 //     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//       "httpss://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
 //   },
 //   {
 //     imdbID: "tt0133093",
 //     Title: "The Matrix",
 //     Year: "1999",
 //     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//       "httpss://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
 //   },
 //   {
 //     imdbID: "tt6751668",
 //     Title: "Parasite",
 //     Year: "2019",
 //     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+//       "httpss://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
 //   },
 // ];
 
@@ -31,7 +31,7 @@ import StarRating from "./StarRating";
 //     Title: "Inception",
 //     Year: "2010",
 //     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//       "httpss://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
 //     runtime: 148,
 //     imdbRating: 8.8,
 //     userRating: 10,
@@ -41,7 +41,7 @@ import StarRating from "./StarRating";
 //     Title: "Back to the Future",
 //     Year: "1985",
 //     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+//       "httpss://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
 //     runtime: 116,
 //     imdbRating: 8.5,
 //     userRating: 9,
@@ -86,7 +86,7 @@ function App() {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
@@ -319,7 +319,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
           const data = await res.json();
           setMovie(data);
